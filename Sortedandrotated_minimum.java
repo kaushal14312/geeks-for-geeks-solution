b6d47896586e0fd7d6f6b5e80824833e9f04cbb9
kaushal14312 <kaushalraj11a@gmail.com>
2024-12-14 16:32:06 +0530

    14december2024

--- Sortedandrotated_minimum.java
+++ Sortedandrotated_minimum.java
@@ -1,3 +1,18 @@
+"Sorted and Rotated Minimum
+Difficulty: EasyAccuracy: 40.57%Submissions: 128K+Points: 2
+A sorted array of distinct elements arr[] is rotated at some unknown point, the task is to find the minimum element in it. 
+
+Examples:
+
+Input: arr[] = [5, 6, 1, 2, 3, 4]
+Output: 1
+Explanation: 1 is the minimum element in the array.
+Input: arr[] = [3, 1, 2]
+Output: 1
+Explanation: Here 1 is the minimum element.
+Input: arr[] = [4, 2, 3]
+Output: 2
+Explanation: Here 2 is the minimum element."
 class Solution {
     public int findMin(int[] arr) {
         int left = 0, right = arr.length - 1;
